04595eb78707bd2c0c0e56756a3ac3c68521b4b6
micweber <michael.h.weber@web.de>
2025-03-27 09:18:06 +0100

    added grpc connection

---
+++ app/udaconnect/grpc_server.py
@@ -0,0 +1,22 @@
+import grpc
+from concurrent import futures
+import time
+import person_pb2
+import person_pb2_grpc
+
+class PersonServiceGRPC(person_pb2_grpc.PersonServiceServicer):
+    def GetPersons(self, request, context):
+        persons = [
+            person_pb2.PersonMessage(id=1, first_name="Alice", last_name="Smith", company_name="TechCorp"),
+            person_pb2.PersonMessage(id=6, first_name="Bob", last_name="Johnson", company_name="SoftInc")
+        ]
+        return person_pb2.PersonMessageList(persons=persons)
+
+
+def serve_grpc():
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    person_pb2_grpc.add_PersonServiceServicer_to_server(PersonServiceGRPC(), server)
+    server.add_insecure_port("[::]:5005")
+    server.start()
+    print("gRPC server is running on port 5005...")
+    server.wait_for_termination()
--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,13 @@
 import os
 
+import threading
+from app.udaconnect.grpc_server import serve_grpc
 from app import create_app
 
 app = create_app(os.getenv("FLASK_ENV") or "test")
 if __name__ == "__main__":
+
+    grpc_thread = threading.Thread(target=serve_grpc, daemon=True)
+    grpc_thread.start()
+
     app.run(debug=True)
