3a4f1a748a24471c67018168aa3128b8f0fe4627
micweber <michael.h.weber@web.de>
2025-03-28 08:57:40 +0100

    implemented database query

--- app/udaconnect/grpc_server.py
+++ app/udaconnect/grpc_server.py
@@ -4,6 +4,7 @@ from concurrent import futures
 import time
 from app.udaconnect import person_pb2
 from app.udaconnect import person_pb2_grpc
+from app.udaconnect.models import Person
 from app.udaconnect.services import PersonService
 import logging
 
@@ -13,26 +14,36 @@ logger = logging.getLogger(__name__)
 class PersonServiceGRPC(person_pb2_grpc.PersonServiceServicer):
     def GetPersons(self, request, context):
 
-        # Database query
-        persons_from_db = PersonService.retrieve_all()
+        from app import create_app
+        from app import db
 
-        # convert into gRPC objects
-        persons = [
-            person_pb2.PersonMessage(
-                id=person.id,
-                first_name=person.first_name,
-                last_name=person.last_name,
-                company_name=person.company_name
-            )
-            for person in persons_from_db
-        ]
+        # Application Context setzen
+        app = create_app()
+        with app.app_context():
+            # Database query
+            persons_from_db = db.session.query(Person).all()
+
+            # convert into gRPC objects
+            persons = [
+                person_pb2.PersonMessage(
+                    id=person.id,
+                    first_name=person.first_name,
+                    last_name=person.last_name,
+                    company_name=person.company_name
+                )
+                for person in persons_from_db
+            ]
+
+            return person_pb2.PersonMessageList(persons=persons)
+        
+     
 
 #
  #       persons = [
   #          person_pb2.PersonMessage(id=1, first_name="Alice", last_name="Smith", company_name="TechCorp"),
    #         person_pb2.PersonMessage(id=6, first_name="Bob", last_name="Johnson", company_name="SoftInc")
     #    ]
-        return person_pb2.PersonMessageList(persons=persons)
+     #   return person_pb2.PersonMessageList(persons=persons)
 
 
 def serve_grpc():
